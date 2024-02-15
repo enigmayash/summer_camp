fa6f2dffe8c0840f23c0aa0ccf0091aa88bfc6aa
enigmayash <112617033+enigmayash@users.noreply.github.com>
2024-02-15 21:45:41 +0530

    Add files via upload

---
+++ summer_camp/data.js
@@ -0,0 +1,22 @@
+const participantsData = [
+	{
+		sport: "Soccer",
+		totalParticipants: 50
+	},
+	{
+		sport: "Basketball",
+		totalParticipants: 30
+	},
+	{
+		sport: "Volleyball",
+		totalParticipants: 40
+	},
+	{
+		sport: "Tennis",
+		totalParticipants: 25
+	},
+	{
+		sport: "Swimming",
+		totalParticipants: 35
+	}
+];
